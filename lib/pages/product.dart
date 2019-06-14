827f3100f9a4aa9bdb94ad723447efe699ae37d6
jstl1025 <jlin7@ualberta.ca>
2019-06-13 19:39:58 -0600

    onUnkownRoute, Alert Dialogue, Modal

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -8,12 +8,39 @@ class ProductPage extends StatelessWidget {
 
   ProductPage(this.title, this.imgURL);
 
+  _showWarningDialogue(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('This action cannot be undone!'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('DISCARD'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              FlatButton(
+                child: Text('CONTINUE'),
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context, true);
+                },
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(onWillPop: (){
-      Navigator.pop(context);
-      return Future.value(false);
-    },
+    return WillPopScope(
+      onWillPop: () {
+        Navigator.pop(context);
+        return Future.value(false);
+      },
       child: Scaffold(
         appBar: AppBar(
           title: Text(title),
@@ -30,7 +57,7 @@ class ProductPage extends StatelessWidget {
                 padding: EdgeInsets.all(10.0),
                 color: Theme.of(context).accentColor,
                 child: Text('Delete'),
-                onPressed: () => Navigator.pop(context, true),
+                onPressed: () => _showWarningDialogue(context),
               ),
             ),
           ],
