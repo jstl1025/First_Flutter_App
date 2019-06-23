e21f323b02df4a5c4cf3b8dec19030df8b791c79
jstl1025 <jlin7@ualberta.ca>
2019-06-23 01:30:14 -0600

    Render products in my product page + reuse product edit form

--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -1,30 +1,31 @@
 import 'package:flutter/material.dart';
 
-import './product_create.dart';
+import './product_edit.dart';
 import './product_list.dart';
 
 class ProductsAdminPage extends StatelessWidget {
   final Function addProduct;
   final Function deleteProduct;
+  final List<Map<String, dynamic>> products;
 
-  ProductsAdminPage(this.addProduct, this.deleteProduct);
+  ProductsAdminPage(this.addProduct, this.deleteProduct, this.products);
 
-  Widget _buildSideDrawer(BuildContext context){
+  Widget _buildSideDrawer(BuildContext context) {
     return Drawer(
-            child: Column(
-              children: <Widget>[
-                AppBar(
-                  automaticallyImplyLeading: false,
-                  title: Text('Choose'),
-                ),
-                ListTile(
-                    leading: Icon(Icons.shop),
-                    title: Text('All Products'),
-                    onTap: () =>
-                        Navigator.pushReplacementNamed(context, '/products')),
-              ],
-            ),
-          );
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            automaticallyImplyLeading: false,
+            title: Text('Choose'),
+          ),
+          ListTile(
+              leading: Icon(Icons.shop),
+              title: Text('All Products'),
+              onTap: () =>
+                  Navigator.pushReplacementNamed(context, '/products')),
+        ],
+      ),
+    );
   }
 
   @override
@@ -50,8 +51,8 @@ class ProductsAdminPage extends StatelessWidget {
           ),
           body: TabBarView(
             children: <Widget>[
-              ProductCreatePage(addProduct),
-              ProductListPage(),
+              ProductEditPage(addProduct: addProduct),
+              ProductListPage(products),
             ],
           )),
     );
