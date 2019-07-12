d829ace78791ff2ff4df944eb36a5c1d7e4d7c1f
jstl1025 <jlin7@ualberta.ca>
2019-07-12 14:59:59 -0600

    Add auto logout

--- lib/scoped-models/connected_products.dart
+++ lib/scoped-models/connected_products.dart
@@ -225,8 +225,9 @@ mixin ProductsModel on ConnectedProductsModel {
 }
 
 mixin UserModel on ConnectedProductsModel {
+  Timer _authTimer;
 
-  User get user{
+  User get user {
     return _authenticatedUser;
   }
 
@@ -263,10 +264,14 @@ mixin UserModel on ConnectedProductsModel {
           id: responseData['localId'],
           email: email,
           token: responseData['idToken']);
+      setAuthTimeout(int.parse(responseData['expiresIn']));
+      final DateTime now = DateTime.now();
+      final DateTime expiryTime = now.add(Duration(seconds: int.parse(responseData['expiresIn'])));
       final SharedPreferences prefs = await SharedPreferences.getInstance();
       prefs.setString('token', responseData['idToken']);
       prefs.setString('userEmail', email);
       prefs.setString('userId', responseData['localId']);
+      prefs.setString('expiryTime', expiryTime.toIso8601String());
     } else if (responseData['error']['message'] == 'EMAIL_EXISTS') {
       message = 'This email already exists';
     } else if (responseData['error']['message'] == 'EMAIL_NOT_FOUND') {
@@ -280,23 +285,38 @@ mixin UserModel on ConnectedProductsModel {
     return {'success': !hasError, 'message': message};
   }
 
-  void autoAuthenticate()async{
+  void autoAuthenticate() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final String token = prefs.getString('token');    
-    if(token != null){
-      final String userEmail = prefs.getString('userEmail');  
+    final String token = prefs.getString('token');
+    final String expiryTimeString = prefs.getString('expiryTime');
+    if (token != null) {
+      final DateTime now = DateTime.now();
+      final parsedExpiryTime = DateTime.parse(expiryTimeString);
+      if(parsedExpiryTime.isBefore(now)){
+        _authenticatedUser = null;
+        notifyListeners();
+        return;
+      }
+      final String userEmail = prefs.getString('userEmail');
       final String userId = prefs.getString('userId');
-      _authenticatedUser = User(id: userId, email: userEmail, token: token);  
+      final int tokenLifeSpan = parsedExpiryTime.difference(now).inSeconds;
+      _authenticatedUser = User(id: userId, email: userEmail, token: token);
+      setAuthTimeout(tokenLifeSpan);
       notifyListeners();
     }
   }
 
-  void logout()async{
+  void logout() async {
     _authenticatedUser = null;
+    _authTimer.cancel();
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.remove('token');
     prefs.remove('userEmail');
     prefs.remove('userId');
+  }
+
+  void setAuthTimeout(int time) {
+    _authTimer = Timer(Duration(seconds: time), logout);
   }
 }
 
